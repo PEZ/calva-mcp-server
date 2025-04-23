d7b9dd123c0e930ddd2fa1c437c319e0a87f9398
PEZ <pez@pezius.com>
2025-04-23 15:31:34 +0200

    Move instructions

--- test-projects/mini-deps/src/mini/playground.clj
+++ test-projects/mini-deps/src/mini/playground.clj
@@ -1,14 +1,6 @@
 (ns mini.playground)
 
-;; NOTICE FOR AI ASSISTANTS:
-;; Before working in this file, please review the interactive programming instructions at:
-;; ../../AI_INTERACTIVE_PROGRAMMING.md
-;;
-;; The document explains the REPL-driven development workflow that should be followed
-;; when solving problems in this Clojure project using Calva's evaluation capabilities.
-
 ;; Example rich comment form ready for use:
 (comment
   ;; Start solving your problem here
-  ;; Remember to evaluate expressions as you go using Calva's REPL integration
   (+ 1 2 3))
