c91f0053e4bc896850300192e80e5f1a5f45659c
PEZ <>
2025-05-03 08:49:03 +0200

    Remove more vsc-et remnants

--- src/calva_mcp_server/ex/ax.cljs
+++ src/calva_mcp_server/ex/ax.cljs
@@ -2,7 +2,6 @@
   (:require
    [calva-mcp-server.app.axs :as app-axs]
    [calva-mcp-server.db.axs :as db-axs]
-   [calva-mcp-server.hello.axs :as hello-axs]
    [calva-mcp-server.integrations.calva.axs :as calva-axs]
    [calva-mcp-server.integrations.node.axs :as node-axs]
    [calva-mcp-server.integrations.vscode.axs :as vscode-axs]
@@ -80,7 +79,6 @@
                             (enrich-from-context context)
                             (enrich-from-state state))]
     (match (namespace action-kw)
-      "hello"        (hello-axs/handle-action state context enriched-action)
       "db"           (db-axs/handle-action state context enriched-action)
       "vscode"       (vscode-axs/handle-action state context enriched-action)
       "node"         (node-axs/handle-action state context enriched-action)
