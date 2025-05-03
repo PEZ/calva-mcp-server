cf3657272a766d02d3c3993140710afba8388f32
PEZ <>
2025-05-03 13:07:48 +0200

    WIP: Enable all tools

--- src/calva_mcp_server/integrations/vscode/tools.cljs
+++ src/calva_mcp_server/integrations/vscode/tools.cljs
@@ -15,7 +15,6 @@
                                                               :message message}}))
 
        :invoke (fn invoke [^js options _token]
-                 (def options options)
                  (p/let [code (.-code (.-input options))
                          ns (.-namespace (.-input options))
                          session-key (.-replSessionKey (.-input options))
@@ -23,84 +22,80 @@
                                                        :calva/code code
                                                        :calva/ns ns
                                                        :calva/repl-session-key session-key})]
-                   (def result result)
                    (vscode/LanguageModelToolResult.
                     #js [(vscode/LanguageModelTextPart.
                           (js/JSON.stringify (clj->js result)))])))})
 
-(comment
-  (def session-key "clj")
-  (def ns "user")
-  (def code ":foo")
-  :rcf)
+(defn GetSymbolInfoTool [dispatch!]
+  #js {:prepareInvocation (fn prepareInvocation [^js options _token]
+                            (let [symbol (-> options .-input .-clojureSymbol)
+                                  message (str "Get info for Clojure symbol: **" symbol "**")]
+                              #js {:invocationMessage "Getting symbol info"
+                                   :confirmationMessages #js {:title "Get Symbol Info"
+                                                              :message message}}))
 
-#_(deftype GetSymbolInfoTool []
-  vscode/LanguageModelTool
-  (prepareInvocation [_this options _token]
-    (let [symbol (.-clojureSymbol (.-input options))
-          message (str "Get info for Clojure symbol: **" symbol "**")]
-      (vscode/LanguageModelToolInvocationMessage. message)))
+       :invoke (fn invoke [^js options _token]
+                 (p/let [symbol (-> options .-input .-clojureSymbol)
+                         ns (-> options .-input .-namespace)
+                         session-key (-> options .-input .-sessionKey)
+                         result (calva/get-symbol-info+ {:ex/dispatch! dispatch! ; Assuming dispatch might be needed later
+                                                         :calva/clojure-symbol symbol
+                                                         :calva/ns ns
+                                                         :calva/repl-session-key session-key})]
+                   (vscode/LanguageModelToolResult.
+                    #js [(vscode/LanguageModelTextPart.
+                          (js/JSON.stringify (clj->js result)))])))})
 
-  (invoke [_this ^js options token]
-    (p/let [symbol (.-clojureSymbol (.-input options))
-            ns (.-namespace (.-input options))
-            session-key (.-sessionKey (.-input options))
-            result (calva/get-symbol-info+ {:calva/clojure-symbol symbol
-                                            :calva/ns ns
-                                            :calva/repl-session-key session-key})]
-      (vscode/LanguageModelToolResult.
-       (array
-        (vscode/LanguageModelTextPart.
-         (js/JSON.stringify (clj->js result))))))))
+(defn GetClojureDocsTool [dispatch!]
+  #js {:prepareInvocation (fn prepareInvocation [^js options _token]
+                            (let [symbol (-> options .-input .-clojureSymbol)
+                                  message (str "Look up docs for Clojure symbol: **" symbol "**")]
+                              #js {:invocationMessage "Looking up ClojureDocs"
+                                   :confirmationMessages #js {:title "Get ClojureDocs Info"
+                                                              :message message}}))
 
-#_(deftype GetClojureDocssTool []
-  vscode/LanguageModelTool
-  (prepareInvocation [_this ^js options _token]
-    (let [symbol (.-clojureSymbol (.-input options))
-          message (str "Look up docs for Clojure symbol: **" symbol "**")]
-      (vscode/LanguageModelToolInvocationMessage. message)))
+       :invoke (fn invoke [^js options _token]
+                 (p/let [symbol (-> options .-input .-clojureSymbol)
+                         result (calva/get-clojuredocs+ {:ex/dispatch! dispatch! ; Assuming dispatch might be needed later
+                                                         :calva/clojure-symbol symbol})]
+                   (vscode/LanguageModelToolResult.
+                    #js [(vscode/LanguageModelTextPart.
+                          (js/JSON.stringify (clj->js result)))])))})
 
-  (invoke [_this options token]
-    (p/let [symbol (.-clojureSymbol (.-input options))
-            result (calva/get-clojuredocs+ {:calva/clojure-symbol symbol})]
-      (vscode/LanguageModelToolResult.
-       (array
-        (vscode/LanguageModelTextPart.
-         (js/JSON.stringify (clj->js result))))))))
+(defn GetOutputLogTool [dispatch!]
+  #js {:prepareInvocation (fn prepareInvocation [^js options _token]
+                            (let [since-line (-> options .-input .-sinceLine)
+                                  message (str "Fetch REPL output from line " since-line)]
+                              #js {:invocationMessage "Fetching REPL output"
+                                   :confirmationMessages #js {:title "Get REPL Output Log"
+                                                              :message message}}))
 
-#_(deftype GetOutputLogTool []
-  vscode/LanguageModelTool
-  (prepareInvocation [_this ^js options _token]
-    (let [since-line (-> options .-input .-sinceLine)
-          message (str "Fetch REPL output from line " since-line)]
-      (vscode/LanguageModelToolInvocationMessage. message)))
-
-  (invoke [_this ^js options token]
-    (p/let [since-line (-> options .-input .-sinceLine)
-            result (calva/get-output {:calva/since-line since-line})]
-      (vscode/LanguageModelToolResult.
-       (array
-        (vscode/LanguageModelTextPart.
-         (js/JSON.stringify (clj->js result))))))))
+       :invoke (fn invoke [^js options _token]
+                 (p/let [since-line (-> options .-input .-sinceLine)
+                         result (calva/get-output {:ex/dispatch! dispatch! ; Assuming dispatch might be needed later
+                                                   :calva/since-line since-line})]
+                   (vscode/LanguageModelToolResult.
+                    #js [(vscode/LanguageModelTextPart.
+                          (js/JSON.stringify (clj->js result)))])))})
 
 (defn register-language-model-tools [dispatch!]
   (cond-> []
     :always
     (conj (vscode/lm.registerTool
            "evaluate_clojure_code"
-           (EvaluateClojureCodeTool. dispatch!)))
+           (EvaluateClojureCodeTool dispatch!)))
 
-    #_#_(calva/exists-get-symbol-info?)
-      (conj (vscode/lm.registerTool
-             "get_symbol_info"
-             (GetSymbolInfoTool.)))
+    (calva/exists-get-symbol-info?)
+    (conj (vscode/lm.registerTool
+           "get_symbol_info"
+           (GetSymbolInfoTool dispatch!)))
 
-    #_#_(calva/exists-get-clojuredocs?)
-      (conj (vscode/lm.registerTool
-             "get_clojuredocs_info"
-             (GetClojureDocssTool.)))
+    (calva/exists-get-clojuredocs?)
+    (conj (vscode/lm.registerTool
+           "get_clojuredocs_info"
+           (GetClojureDocsTool dispatch!)))
 
-    #_#_(calva/exists-on-output?)
-      (conj (vscode/lm.registerTool
-             "get_repl_output_log"
-             (GetOutputLogTool.)))))
+    (calva/exists-on-output?)
+    (conj (vscode/lm.registerTool
+           "get_repl_output_log"
+           (GetOutputLogTool dispatch!)))))
