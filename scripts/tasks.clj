ec223fea5b3eb47333a9c2324d3f7db4213a5a30
PEZ <>
2025-05-07 20:32:15 +0200

    Add task for both package and test

--- scripts/tasks.clj
+++ scripts/tasks.clj
@@ -34,6 +34,16 @@
     (println "Use --force to actually bump the version")))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn run-e2e-tests-with-vsix! [{:keys [vsix]}]
+  (println "Running end-to-end tests using vsix:" vsix)
+  (util/shell false "node" "./e2e-test-ws/launch.js" (str "--vsix=" vsix)))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn run-e2e-tests-from-working-dir! []
+  (println "Running end-to-end tests using working directory")
+  (util/shell false "node" "./e2e-test-ws/launch.js"))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn package-pre-release! [{:keys [slug dry]}]
   (let [current-version (-> (util/sh false "node" "-p" "require('./package').version")
                             :out string/trim)
@@ -43,23 +53,25 @@
                       :out string/trim)
         random-slug (util/random-slug 2)
         slugged-branch (string/replace slug #"/" "-")
-        version (str current-version "-" slugged-branch "-" commit-id "-" random-slug)]
+        version (str current-version "-" slugged-branch "-" commit-id "-" random-slug)
+        package-name "calva-backseat-driver"
+        vsix-file (str package-name "-" version ".vsix")]
     (println "Current version:" current-version)
     (println "HEAD Commit ID:" commit-id)
     (println "Packaging pre-release...")
     (util/shell dry "npm" "version" "--no-git-tag-version" version)
     (util/shell dry "npx" "vsce" "package" "--pre-release")
-    (util/shell dry "npm" "version" "--no-git-tag-version" current-version)))
+    (util/shell dry "npm" "version" "--no-git-tag-version" current-version)
+    {:vsix-file vsix-file}))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defn run-e2e-tests-with-vsix! [{:keys [vsix]}]
-  (println "Running end-to-end tests using vsix:" vsix)
-  (util/shell false "node" "./e2e-test-ws/launch.js" (str "--vsix=" vsix)))
-
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defn run-e2e-tests-from-working-dir! []
-  (println "Running end-to-end tests using working directory")
-  (util/shell false "node" "./e2e-test-ws/launch.js"))
+(defn package-and-test-prerelease! [{:keys [slug dry]}]
+  (let [opts {:slug slug :dry dry}
+        vsix-result (package-pre-release! opts)
+        vsix-file (:vsix-file vsix-result)]
+    (println "Created VSIX:" vsix-file)
+    (println "Running e2e tests on the packaged VSIX...")
+    (run-e2e-tests-with-vsix! {:vsix vsix-file})))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn launch-with-vsix! [{:keys [vsix]}]
